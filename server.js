87b0285ef7b3974ca320f6115a22a67bf2587670
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-05-12 20:15:07 -0700

    update backend

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use(require("./routes"));
